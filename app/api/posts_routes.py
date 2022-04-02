df0d14e0d11c7f46222e9b743ec09799bd930b80
nick-esqueda <nick_esqueda@outlook.com>
2022-04-02 01:02:56 -0500

    pseudo coded post routes

--- app/api/posts_routes.py
+++ app/api/posts_routes.py
@@ -3,9 +3,42 @@ from flask import Blueprint, render_template, redirect, url_for
 from app.models import db, Post
 
 
-bp = Blueprint('posts', __name__, url_prefix="/")
+bp = Blueprint('posts', __name__, url_prefix="/posts")
 
 
 @bp.route('/')
-def index():
+def get_posts():
+  # get all posts and return them as json
   return 'nah'
+  
+@bp.route('/<int:postId>')
+def get_post():
+  # get the postId from params and query DB, send back as json
+  pass
+  
+@bp.route('/', methods=["POST"])
+def create_post():
+  # get the post from request body
+  # create instance of post passing in info
+  # save to session and send the new post from db back 
+  pass
+
+@bp.route('/<int:postId>', methods=["PUT"])
+def edit_post():
+  # get post from req body
+  # query db for the existing post
+  # set all properties equal to request's post
+  # save to session
+  # send back new post
+  pass
+
+@bp.route('/<int:postId>', methods=["DELETE"])
+def delete_post():
+  # get userId from the request body
+  # query for post in db with postId
+  # ^ post = Post.query.get(postId)
+  # check if the post's userId == userId in body
+  # delete the post 
+  # ^ db.session.delete(post)
+  # send back the postId and userId as json
+  pass
