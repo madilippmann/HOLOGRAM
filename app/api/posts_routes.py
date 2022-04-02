8bc771647e05ddb509b50db3f26c8044caf7d62a
nick-esqueda <nick_esqueda@outlook.com>
2022-04-02 18:47:24 -0500

    basic setup for comments routes

--- app/api/posts_routes.py
+++ app/api/posts_routes.py
@@ -6,7 +6,7 @@ from app.api.utils import validation_errors_to_error_messages
 
 posts_routes = Blueprint('posts', __name__)
 
-
+# POST ROUTES ##################################################################################
 @posts_routes.route('/')
 def get_feed_posts():
     # FIX FIX FIX only get posts of users that session user follows
@@ -14,11 +14,12 @@ def get_feed_posts():
     posts = [post.to_dict() for post in posts]
     return jsonify(posts)
 
+
 @posts_routes.route('/<int:postId>/')
 def get_post(postId):
   post = Post.query.get(postId).to_dict()
-  # ??? do posts need to be converted to json ???
   return jsonify(post)
+
 
 @posts_routes.route('/', methods=["POST"])
 def create_post():
@@ -46,8 +47,6 @@ def edit_post(postId):
     post = request.get_json()
     form = EditPostForm()
 
-    # Get the csrf_token from the request cookie and put it into the
-    # form manually to validate_on_submit can be used
     form['csrf_token'].data = request.cookies['csrf_token']
 
     if form.validate_on_submit():
@@ -61,7 +60,6 @@ def edit_post(postId):
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
-
 @posts_routes.route('/<int:postId>/', methods=["DELETE"])
 def delete_post(postId):
   post = Post.query.get(postId)
@@ -73,3 +71,6 @@ def delete_post(postId):
     return jsonify(postId)
   else:
     return jsonify('invalid'), 401
+
+
+# COMMENT ROUTES #############################################################################
