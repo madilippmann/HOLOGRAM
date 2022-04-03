1809a7dd3fc227d7d9e10fe92fcf7217db822119
Kevin Pravia Mayorga <keserrano00@gmail.com>
2022-04-03 16:06:25 -0600

    add priority concern toggle for following user

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
+import * as userActions from '../store/users'
+
 function User() {
+  const dispatch = useDispatch();
   const [user, setUser] = useState({});
   const { userId }  = useParams();
+  const sessionUser = useSelector(state => state.session.user);
+
+  let isFollowed = user?.followers?.find(user => user.id === sessionUser.id) ? true : false
 
   useEffect(() => {
     if (!userId) {
@@ -20,6 +27,10 @@ function User() {
     return null;
   }
 
+  const toggleFollow = (e) => {
+    dispatch(userActions.toggleUserFollow(+userId))
+  }
+
   return (
     <ul>
       <li>
@@ -30,6 +41,12 @@ function User() {
       </li>
       <li>
         <strong>Email</strong> {user.email}
+      </li>
+      <li>
+        {isFollowed
+          ? <button onClick={toggleFollow}>Unfollow</button>
+          : <button onClick={toggleFollow}>Follow</button>
+        }
       </li>
     </ul>
   );
