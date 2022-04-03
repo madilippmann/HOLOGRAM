198ee78397956e244bffab62ab8642307b0c9839
Kevin Pravia Mayorga <keserrano00@gmail.com>
2022-04-03 16:18:39 -0600

    add toggleUserFollow

--- react-app/src/store/users.js
+++ react-app/src/store/users.js
@@ -24,7 +24,9 @@ export const fetchUser = userId => async dispatch => {
   }
 }
 
-// export const fetchUsers = () =>
+export const toggleUserFollow = () => {
+
+}
 
 // REDUCER ************************************************
 const usersReducer = (state = {}, action) => {
