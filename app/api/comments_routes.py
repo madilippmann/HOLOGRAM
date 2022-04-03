11c1c326a1382cc4b42e4d490daa1323bba7b831
nick-esqueda <nick_esqueda@outlook.com>
2022-04-02 19:40:56 -0500

    successfully tested GET /api/posts/:postId/comments

--- app/api/comments_routes.py
+++ app/api/comments_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request, jsonify
 from app.forms import CreatePostForm, EditPostForm
-from app.models import db, Post
+from app.models import db, Comment
 from app.api.utils import validation_errors_to_error_messages
 
 comments_routes = Blueprint('comments', __name__)
@@ -9,9 +9,6 @@ comments_routes = Blueprint('comments', __name__)
 # ROUTES #############################################################################
 @comments_routes.route('/<int:postId>/comments/')
 def get_comments(postId):
-    # TODO MAKE COMMENT MODEL FIRST
-    
-    # comments = Comment.query.all()
-    # posts = [comment.to_dict() for comment in comments]
-    # return jsonify(comments)
-    pass
+    comments = Comment.query.filter(Comment.postId == postId).all()
+    comments = [comment.to_dict() for comment in comments]
+    return jsonify(comments)
