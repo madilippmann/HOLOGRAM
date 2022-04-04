11d3ca90514cbc30ed1d0731c9d0730c3cc605d2
nick-esqueda <nick_esqueda@outlook.com>
2022-04-04 13:23:36 -0500

    added and styled footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -8,8 +8,25 @@ export default function Footer() {
 
 
 	return (
-		<div id='footer'>
-			bruh
-		</div>
+		<footer id='footer'>
+			<div id='information-button'>
+				{/* open information modal */}
+				<button type='button'>i</button>
+			</div>
+			<div id='languages'>
+				<ul>
+					<li>React</li>
+					<li>Redux</li>
+					<li>Python</li>
+					<li>Flask</li>
+					<li>PostgreSQL</li>
+					<li>SQLAlchemy</li>
+					<li>Heroku</li>
+					<li>AWS</li>
+					<li>HTML</li>
+					<li>CSS</li>
+				</ul>
+			</div>
+		</footer>
 	);
 }
