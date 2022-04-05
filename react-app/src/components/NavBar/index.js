043842cdad1eb6eca85528277a95c0a8ab18b716
Madi Lippmann <madi.lippmann@gmail.com>
2022-04-05 03:30:33 +0000

    Adjusted title color and navicon colors

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -21,7 +21,7 @@ const NavBar = () => {
 								alt="logo"
 								className="nav__logo"
 							/>
-							<h1><span style={{ color: 'var(--color-purple)' }}>holo</span><span style={{ color: '#F1F1F1' }}>gram</span></h1>
+							<h1><span style={{ color: 'var(--color-purple)' }}>holo</span><span style={{ color: 'rgb(215, 155, 134)' }}>gram</span></h1>
 						</div>
 					</Link>
 
