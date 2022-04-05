beb1b042ac0e6dbbf368eb8d5910813a90575160
nick-esqueda <nick_esqueda@outlook.com>
2022-04-05 13:21:51 -0500

    modified ProfileIcon to use defaultProfileImage

--- react-app/src/components/ProfileIcon/index.js
+++ react-app/src/components/ProfileIcon/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import defaultProfileImage from '../../static/default-profile-image.png'
 import './ProfileIcon.css'
 
 export default function ProfileIcon({ user }) {
@@ -7,7 +8,7 @@ export default function ProfileIcon({ user }) {
         <div className='profile-icon-border'>
             <img
                 className='profile-icon'
-                src={user.profileImageUrl ? user.profileImageUrl : "https://images.unsplash.com/photo-1557682224-5b8590cd9ec5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGdyYWRpZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"}
+				src={user.profileImageUrl !== '/default-profile-image.png' ? user.profileImageUrl : defaultProfileImage}
                 alt="profile-picture"
             />
         </div>
