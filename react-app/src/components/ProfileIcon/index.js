cee0a024f5a8e17900d7fa8fb873682a2898103a
nick-esqueda <nick_esqueda@outlook.com>
2022-04-05 16:01:44 -0500

    set ProfileIcon to navigate to user's profile page onClick

--- react-app/src/components/ProfileIcon/index.js
+++ react-app/src/components/ProfileIcon/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 import defaultProfileImage from '../../static/default-profile-image.png'
 
 import './ProfileIcon.css'
 
 export default function ProfileIcon({ user }) {
+    const history = useHistory();
+    
     return (
-        <div className='profile-icon-border'>
+        <div 
+            className='profile-icon-border' 
+            onClick={(e) => history.push(`/${user.handle}`)}
+        >
             <img
                 className='profile-icon'
 				src={user.profileImageUrl !== '/default-profile-image.png' ? user.profileImageUrl : defaultProfileImage}
