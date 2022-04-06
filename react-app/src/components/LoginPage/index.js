288eb6e6d0b3ac4c08a718cc76741a1676d56564
Madi Lippmann <madi.lippmann@gmail.com>
2022-04-06 13:13:09 -0700

    Finished styling for login and signup pages

--- react-app/src/components/LoginPage/index.js
+++ react-app/src/components/LoginPage/index.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
 
+import '../SignUpPage'
+import logo from '../../static/hologram-logo.png'
 export default function LoginPage() {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    document.body.style.background = "linear-gradient(135deg, rgba(122,123,255,1) 12%, rgba(255,201,181,1) 91%)";
+    document.body.style.minHeight = "100vh"
+    document.body.style.margin = "auto"
+    document.body.style.display = "flex"
+    document.body.style.justifyContent = "center"
+    document.body.style.alignItems = "center"
+    document.body.style.width = "100vw"
+    return () => {
+      document.body.style.backgroundColor = "rgb(252, 248, 247)";
+      document.body.style.minHeight = "1200px"
+
+    };
+  }, []);
+
 
   const onLogin = async (e) => {
     e.preventDefault();
@@ -30,34 +48,58 @@ export default function LoginPage() {
     return <Redirect to='/' />;
   }
 
+  const loginDemoUser = () => {
+
+  }
+
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className='signup-login-container'>
+      <div className='signup-login-form-container'>
+        <form onSubmit={onLogin} className='signup-login-form'>
+          <div className='logo-title-container'>
+            <img id='signup-login-logo' src={logo} alt='HOLOGRAM logo' />
+            <h1><span style={{ color: 'var(--color-purple)' }}>holo</span><span style={{ color: 'rgb(215, 155, 134)' }}>gram</span></h1>
+          </div>
+
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div className='signup-login-input-container'>
+            <input
+              name='email'
+              type='text'
+              placeholder='Email'
+              value={email}
+              className='signup-login-input'
+              onChange={updateEmail}
+              autoComplete='new-password'
+            />
+          </div>
+          <div className='signup-login-input-container'>
+            <input
+              name='password'
+              type='password'
+              placeholder='Password'
+              value={password}
+              className='signup-login-input'
+              onChange={updatePassword}
+            />
+
+          </div>
+          <button className='signup-login-submit-button' type='submit'>login</button>
+          <div className='demo-and-signup'>
+            <Link to='/sign-up'>
+              <p className='login-link'>
+                sign up
+              </p>
+            </Link>
+            <button className='demo-user-button' type='button' onClick={loginDemoUser}>demo</button>
+
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    </div>
   );
 };
--- react-app/src/components/SignUpPage/index.js
+++ react-app/src/components/SignUpPage/index.js
@@ -4,167 +4,177 @@ import { Link, Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
 
 import './SignUpPage.css'
+import logo from '../../static/hologram-logo.png'
+
+
 
 export default function SignUpPage() {
-  const [errors, setErrors] = useState([]);
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [handle, setHandle] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+	const [errors, setErrors] = useState([]);
+	const [firstName, setFirstName] = useState('')
+	const [lastName, setLastName] = useState('')
+	const [handle, setHandle] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [repeatPassword, setRepeatPassword] = useState('');
+	const user = useSelector(state => state.session.user);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    document.body.style.background = "linear-gradient(135deg, rgba(122,123,255,1) 12%, rgba(255,201,181,1) 91%)";
-    document.body.style.minHeight = "100vh"
-    document.body.style.margin = "0"
-    document.body.style.display = "flex"
-    document.body.style.justifyContent = "center"
-    document.body.style.alignItems = "center"
-    document.body.style.width = "100%"
-    // return () => {
-    //   document.body.style.backgroundColor = "rgb(252, 248, 247)";
-    //   document.body.style.minHeight = "1200px"
+	useEffect(() => {
+		document.body.style.background = "linear-gradient(135deg, rgba(122,123,255,1) 12%, rgba(255,201,181,1) 91%)";
+		document.body.style.minHeight = "100vh"
+		document.body.style.margin = "auto"
+		document.body.style.display = "flex"
+		document.body.style.justifyContent = "center"
+		document.body.style.alignItems = "center"
+		document.body.style.width = "100vw"
+		return () => {
+			document.body.style.backgroundColor = "rgb(252, 248, 247)";
+			document.body.style.minHeight = "1200px"
 
-    // };
-  }, []);
-
-
-  const onSignUp = async (e) => {
-    e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(firstName, lastName, handle, email, password));
-      if (data) {
-        setErrors(data)
-      }
-    }
-  };
-
-  const updateFirstName = (e) => {
-    setFirstName(e.target.value);
-  };
-
-  const updateLastName = (e) => {
-    setLastName(e.target.value);
-  };
-
-  const updateHandle = (e) => {
-    setHandle(e.target.value);
-  };
-
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
-
-  if (user) {
-    return <Redirect to='/' />;
-  }
+		};
+	}, []);
 
 
-  return (
-    <div className='signup-login-container'>
+	const onSignUp = async (e) => {
+		e.preventDefault();
+		if (password === repeatPassword) {
+			const data = await dispatch(signUp(firstName, lastName, handle, email, password));
+			if (data) {
+				setErrors(data)
+			}
+		}
+	};
 
-      <form
-        className='signup-login-form'
-        onSubmit={onSignUp}
-      >
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <div className='signup-login-input-container'>
-          {/* <label>First Name</label> */}
-          <input
-            type='text'
-            name='firstName'
-            onChange={updateFirstName}
-            value={firstName}
-            className='signup-login-input'
-            placeholder='first name'
-          // autoComplete='new-password'
-          ></input>
-        </div>
-        <div className='signup-login-input-container'>
-          {/* <label>Last Name</label> */}
-          <input
-            type='text'
-            name='handle'
-            onChange={updateLastName}
-            value={lastName}
-            className='signup-login-input'
-            placeholder='last name'
-            autoComplete='new-password'
-          ></input>
-        </div>
-        <div className='signup-login-input-container'>
-          {/* <label>User Handle</label> */}
-          <input
-            type='text'
-            name='handle'
-            onChange={updateHandle}
-            value={handle}
-            className='signup-login-input'
-            placeholder='handle'
-            autoComplete='new-password'
-          ></input>
-        </div>
-        <div className='signup-login-input-container'>
-          {/* <label>Email</label> */}
-          <input
-            type='text'
-            name='email'
-            onChange={updateEmail}
-            value={email}
-            className='signup-login-input'
-            placeholder='email'
-            autoComplete='new-password'
-          ></input>
-        </div>
+	const updateFirstName = (e) => {
+		setFirstName(e.target.value);
+	};
 
-        <div className='signup-login-input-container'>
-          {/* <label>Password</label> */}
-          <input
-            type='password'
-            name='password'
-            onChange={updatePassword}
-            value={password}
-            className='signup-login-input'
-            placeholder='password'
-          // autoComplete='new-password'
-          ></input>
-        </div>
+	const updateLastName = (e) => {
+		setLastName(e.target.value);
+	};
 
-        <div className='signup-login-input-container'>
-          {/* <label>Repeat Password</label> */}
-          <input
-            type='password'
-            name='repeat_password'
-            onChange={updateRepeatPassword}
-            value={repeatPassword}
-            required={true}
-            className='signup-login-input'
-            placeholder='confirm password'
-          // autoComplete='new-password'
-          ></input>
-        </div>
-        <button type='submit'>Sign Up</button>
-        <Link to='/login'>
-          <p className='login-link'>
-            Login
-          </p>
-        </Link>
-      </form>
-    </div>
-  );
+	const updateHandle = (e) => {
+		setHandle(e.target.value);
+	};
+
+	const updateEmail = (e) => {
+		setEmail(e.target.value);
+	};
+
+	const updatePassword = (e) => {
+		setPassword(e.target.value);
+	};
+
+	const updateRepeatPassword = (e) => {
+		setRepeatPassword(e.target.value);
+	};
+
+	if (user) {
+		return <Redirect to='/' />;
+	}
+
+
+	return (
+		<div className='signup-login-container'>
+
+			<div className='signup-login-form-container'>
+				<form
+					className='signup-login-form'
+					onSubmit={onSignUp}
+				>
+					<div className='logo-title-container'>
+						<img id='signup-login-logo' src={logo} alt='HOLOGRAM logo' />
+						<h1><span style={{ color: 'var(--color-purple)' }}>holo</span><span style={{ color: 'rgb(215, 155, 134)' }}>gram</span></h1>
+
+					</div>
+					<div>
+						{errors.map((error, ind) => (
+							<div key={ind}>{error}</div>
+						))}
+					</div>
+
+					<div className='signup-login-input-container'>
+						<input
+							type='text'
+							name='firstName'
+							onChange={updateFirstName}
+							value={firstName}
+							className='signup-login-input'
+							placeholder='first name'
+						></input>
+					</div>
+
+					<div className='signup-login-input-container'>
+						<input
+							type='text'
+							name='handle'
+							onChange={updateLastName}
+							value={lastName}
+							className='signup-login-input'
+							placeholder='last name'
+							autoComplete='new-password'
+						></input>
+					</div>
+
+					<div className='signup-login-input-container'>
+						<input
+							type='text'
+							name='handle'
+							onChange={updateHandle}
+							value={handle}
+							className='signup-login-input'
+							placeholder='handle'
+							autoComplete='new-password'
+						></input>
+					</div>
+
+					<div className='signup-login-input-container'>
+						<input
+							type='text'
+							name='email'
+							onChange={updateEmail}
+							value={email}
+							className='signup-login-input'
+							placeholder='email'
+							autoComplete='new-password'
+						></input>
+					</div>
+
+					<div className='signup-login-input-container'>
+						<input
+							type='password'
+							name='password'
+							onChange={updatePassword}
+							value={password}
+							className='signup-login-input'
+							placeholder='password'
+						></input>
+					</div>
+
+					<div className='signup-login-input-container'>
+						<input
+							type='password'
+							name='repeat_password'
+							onChange={updateRepeatPassword}
+							value={repeatPassword}
+							required={true}
+							className='signup-login-input'
+							placeholder='confirm password'
+						></input>
+					</div>
+					<button className='signup-login-submit-button' type='submit'>sign up</button>
+					<div className='border-top'>
+						<Link to='/login'>
+							<p className='login-link'>
+								login
+							</p>
+						</Link>
+
+					</div>
+				</form>
+
+			</div>
+
+		</div>
+	);
 };
