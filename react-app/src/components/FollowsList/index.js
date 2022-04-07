95008e1746b80966f4b5d3242587b83fd943064e
Madi Lippmann <madi.lippmann@gmail.com>
2022-04-07 04:48:11 +0000

    Follows dropdown working - still need to adjust width to handle text overflow

---
+++ react-app/src/components/FollowsList/index.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+import defaultProfileImage from '../../static/default-profile-image.png'
+
+import './FollowsList.css'
+
+const FollowsList = ({ follows }) => {
+    return (
+        <>
+            {follows.map((follow) => {
+                return (
+                    <Link to={`/${follow.handle}`}>
+                        <div key={follow.id} className='selected-dropdown-user'>
+                            <div className='user-profile-avatar'>
+                                <img className='follows-avatar-image' src={follow.profileImageUrl !== '/default-profile-image.png' ? follow.profileImageUrl : defaultProfileImage} />
+                            </div>
+                            <div className='justify-left'>
+                                <p className='follows-name'>{follow.firstName} {follow.lastName}</p>
+                                <p className='follows-handle'>{follow.handle}</p>
+                            </div>
+                        </div>
+                    </Link>
+                )
+            })}
+
+        </>
+    );
+}
+
+export default FollowsList;
