d3139466ae710c68d0a84d7894940044a9890827
nick-esqueda <nick_esqueda@outlook.com>
2022-04-08 00:53:59 -0500

    copied over previously created search slice of state

---
+++ react-app/src/store/search.js
@@ -0,0 +1,41 @@
+// ACTION VARIABLES ***************************************
+const LOAD_SEARCH = 'session/LOAD_SEARCH';
+
+
+// ACTION CREATORS ****************************************
+const loadSearch = (results) => {
+  return {
+    type: LOAD_SEARCH,
+    results
+  }
+}
+
+
+
+// THUNK ACTION CREATORS **********************************
+export const fetchQuery = query => async dispatch => {
+  const res = await fetch(`/api/search/${query}`);
+
+  if (res.ok) {
+    const results = await res.json();
+    dispatch(loadSearch(results));
+    return results;
+  }
+}
+
+
+
+// REDUCER ************************************************
+export default searchReducer = (state = [], action) => {
+  let newState;
+  switch (action.type) {
+    case LOAD_SEARCH: {
+      newState = [ ...state, ...action.results ];
+      return newState;
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
