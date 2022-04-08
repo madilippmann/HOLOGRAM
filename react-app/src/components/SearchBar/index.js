a1104b246b68796625fc76ad6d936768477270d6
nick-esqueda <nick_esqueda@outlook.com>
2022-04-08 09:05:12 -0500

    WIP search filtering logic

--- react-app/src/components/SearchBar/index.js
+++ react-app/src/components/SearchBar/index.js
@@ -14,8 +14,9 @@ const options = {
 	useExtendedSearch: true,
 	keys: [
 		{ name: 'caption', weight: 0.9 },
-		// {name: 'User.username', weight: 0.5},
-		// {name: 'description', weight: 0.2},
+		{ name: 'handle', weight: 0.6 },
+		{ name: 'firstName', weight: 0.4 },
+		{ name: 'lastName', weight: 0.4 },
 	]
 }
 
@@ -24,13 +25,15 @@ export default function SearchBar() {
 	const dispatch = useDispatch();
 	const history = useHistory();
 	const posts = useSelector(state => Object.values(state.posts));
-	const dbQueryResults = useSelector(state => state.search);
+	// const dbQueryResults = useSelector(state => state.search);
 	const [query, setQuery] = useState('');
 	const [results, setResults] = useState(!posts.length ? [] : posts);
 	const [showMenu, setShowMenu] = useState(false);
+	console.log(results);
+
 
 	useEffect(() => {
-		// if (!query) return;
+		if (!query) return;
 
 		// let timer;
 		// if (dbQueryResults.length < 20 && query.length > 1) {
@@ -42,38 +45,52 @@ export default function SearchBar() {
 		// 	}, 400);
 
 		// } else {
-		// const fuse = new Fuse(dbQueryResults, options);
-		const fuse = new Fuse(posts, options);
-		const fuseResults = fuse.search(query);
-		setResults(fuseResults);
+		// 	const fuse = new Fuse(dbQueryResults, options);
+		// 	const fuseResults = fuse.search(query);
+		// 	setResults(fuseResults);
 		// }
 
-		// return () => clearTimeout(timer);
+		let timer;
+
+		const fuse = new Fuse(posts, options);
+		const stateResults = fuse.search(query);
+		setResults(stateResults);
+
+		if (results.length < 20 && query.length > 1) {
+			timer = setTimeout(async () => {
+				const dbQueryResults = await dispatch(fetchQuery(query));
+				const fuse = new Fuse(dbQueryResults, options);
+				const fuseResults = fuse.search(query);
+				setResults(prevResults => prevResults.concat(fuseResults));
+			}, 400);
+		}
+
+		return () => clearTimeout(timer);
 	}, [query]);
-	
-	useEffect(() => {
-		(async () => {
-			const res = await dispatch(fetchQuery('an'));
-			console.log(res);
-		})()
-	}, [query]);
-	
+
+	// useEffect(() => {
+	// 	(async () => {
+	// 		const res = await dispatch(fetchQuery('an'));
+	// 		console.log(res);
+	// 	})()
+	// }, [query]);
+
 	const openMenu = () => {
 		if (showMenu) return;
 		document.querySelector('.search');
 		setShowMenu(true);
 	};
-	
+
 	useEffect(() => {
 		if (!showMenu) return;
-		
+
 		const closeMenu = () => {
 			document.querySelector('.search');
 			setShowMenu(false);
 		};
-		
+
 		document.addEventListener('click', closeMenu);
-		
+
 		return () => {
 			setShowMenu(false);
 			document.removeEventListener("click", closeMenu);
@@ -103,11 +120,22 @@ export default function SearchBar() {
 			{showMenu && (
 				<div className='search-filter'>
 					<div id="search-message" onClick={onSubmit}>press enter to search for "{query}"...</div>
-					{results.map((result, i) => (
-						<span key={i} onClick={() => history.push(`/posts/${result.item.id}`)} className="search-item">
-							{result.item.id}
-						</span>
-					))}
+					{results.map((result, i) => {
+						{
+							result.item.handle !== undefined && (
+								<span key={i} onClick={() => history.push(`/${result.item.handle}`)} className="search-item">
+									{result.item.handle}
+								</span>
+							)
+						}
+						{
+							result.item.caption !== undefined && (
+								<span key={i} onClick={() => history.push(`/posts/${result.item.id}`)} className="search-item">
+									{result.item.caption}
+								</span>
+							)
+						}
+					})}
 				</div>
 			)}
 		</div>
