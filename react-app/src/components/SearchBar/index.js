c0b7724f8918075fbd85986da1bd99e9ae3af0a4
nick-esqueda <nick_esqueda@outlook.com>
2022-04-08 01:04:09 -0500

    installed fuse.js, pasted over previously created search logic

--- react-app/src/components/SearchBar/index.js
+++ react-app/src/components/SearchBar/index.js
@@ -6,12 +6,49 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './SearchBar.css';
 
+import Fuse from 'fuse.js';
+const options = {
+  includeScore: true,
+  findAllMatches: true,
+  useExtendedSearch: true,
+  keys: [
+    {name: 'title', weight: 0.9},
+    {name: 'User.username', weight: 0.5},
+    {name: 'description', weight: 0.2},
+  ]
+}
+
+
 export default function SearchBar() {
 	const dispatch = useDispatch();
 	const history = useHistory();
+	const posts = useSelector(state => Object.values(state.posts));
+	const dbQueryResults = useSelector(state => state.search);
 	const [query, setQuery] = useState('');
+	const [results, setResults] = useState(!posts.length ? [] : posts);
 	const [showMenu, setShowMenu] = useState(false);
 
+	useEffect(() => {
+		if (!query) return;
+		
+		let timer;
+		if (dbQueryResults.length < 20 && query.length > 1) {
+		  timer = setTimeout(async () => {
+			const dbResults = await dispatch(fetchQuery(query));
+			const fuse = new Fuse(dbResults, options);
+			const fuseResults = fuse.search(query);
+			setResults(fuseResults);
+		  }, 400);
+		  
+		} else {
+		  const fuse = new Fuse(dbQueryResults, options);
+		  const fuseResults = fuse.search(query);
+		  setResults(fuseResults);
+		}
+	
+		return () => clearTimeout(timer);
+	  }, [query]);
+	
 	const closeMenu = (e) => {
 		setShowMenu(false);
 	}
@@ -25,7 +62,6 @@ export default function SearchBar() {
 
 
 	return (
-
 		<div className='search'>
 			<form className="search__form" onSubmit={onSubmit}>
 				<input type="text" placeholder="search"
