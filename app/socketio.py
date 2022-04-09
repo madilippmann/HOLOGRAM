ec78dd1a66682d85005140bdf1083f6763b27ba8
nick-esqueda <nick_esqueda@outlook.com>
2022-04-09 13:45:07 -0500

    WIP room on_join route

--- app/socketio.py
+++ app/socketio.py
@@ -14,39 +14,31 @@ socketio = SocketIO(cors_allowed_origins="*", logger=True, engineio_logger=True)
 
 @socketio.on('connect')
 def test_connect(auth):
-    # print(session, 'sess\n\n\n')
-    # userId = session["_user_id"]
-    # User.sids[userId] = request.sid
-    # print(User.sids)
-
     emit('my response', {'data': 'Connected'})
+
+@socketio.on('message')
+def handle_message(message):
+    emit('message', message, broadcast=True)
+    return None
 
 @socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
 
+# ROOM STUFF
+@socketio.on('on_join')
+def on_join(data):
+    handle = data['handle']
+    room = data['room']
+    print('\n\n\n\nHANDLE, THEN ROOM: ', handle, room,'\n\n\n\n')
+    join_room(room)
+    
+    # data = {
+    #     ['content']: f"{handle} has entered the room."
+    # }
 
-@socketio.on('message')
-def handle_message(message):
-    # print('\n\n\n\n\n\n', request.sid, message, '\n\n\n\n\n\n')
-    # room = data['room']
+    emit('open_room', {'room': room}, broadcast=True)
 
-    # print(User.sids)
-
-    emit('message', message, broadcast=True)
-    return None
-
-# @socketio.on('on_join')
-# def on_join(data):
-#     handle = data['handle']
-#     room = data['room']
-#     join_room(room)
-#     print('\n\n\n\nJOINING:', handle, room,'\n\n\n\n')
-#     data = {
-#         ['content']: f"{handle} has entered the room."
-#     }
-
-#     send(data, to=room)
 
 # @socketio.on('on_leave')
 # def on_leave(data):
@@ -60,43 +52,20 @@ def handle_message(message):
 
 
 
-# # handle chat messages
-# @socketio.on('connect')
-# def on_connect():
-#     print('\n\n\n\n\n\n\n\nuser connected\n\n\n\n\n\n\n')
-#     retrieve_active_users()
 
-
-# def retrieve_active_users():
-#     emit('retrieve_active_users', broadcast=True)
-
-
-# @socketio.on('activate_user')
-# def on_active_user(data):
-#     user = data.get('username')
-#     emit('user_activated', {'user': user}, broadcast=True)
-
-
-# @socketio.on('deactivate_user')
-# def on_inactive_user(data):
-#     user = data.get('username')
-#     emit('user_deactivated', {'user': user}, broadcast=True)
 
 
 # @socketio.on('join_room')
 # def on_join(data):
 #     room = data['room']
-#     print('joinnning hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', data)
 #     join_room(room)
 #     emit('open_room', {'room': room}, broadcast=True)
 
 # @socketio.on("leave_room")
 # def leave(data):
-#     print('leaving hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', data)
 #     leave_room(data['room'])
 
 # @socketio.on('message')
 # def on_chat_sent(data):
-#     print('data issssss hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', data)
 #     room = data['room']
 #     send({'id':data['id'],'dm_room_id': data['dm_room_id'] , 'channel_id':data['channel_id'], 'content': data['content'], 'created_at': data['created_at'], 'room':data['room'], 'sender_username': data['sender_username'], 'sender_profile_picture': data['sender_profile_picture'] }, room=data['room'],)
