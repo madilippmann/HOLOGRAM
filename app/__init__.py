150d4b064b889ac113c8b53a9ef15425e05e1f14
Madi Lippmann <madi.lippmann@gmail.com>
2022-04-08 23:51:56 -0700

    Basic sockets working, save before passing off to Nick

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
-from flask_socketio import SocketIO, emit, send
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
@@ -73,10 +73,25 @@ def test_disconnect():
 @socketio.on('message', namespace='/messages')
 def handle_message(message):
     print('\n\n\n', message, '\n\n\n')
+    print('\n\n\n', request.sid, '\n\n\n')
+
     emit('message', message, broadcast=True)
     # send(message, broadcast=True)
     return None
 
+# @socketio.on('join')
+# def on_join(data):
+#     username = data['username']
+#     room = data['room']
+#     join_room(room)
+#     send(username + ' has entered the room.', to=room)
+
+# @socketio.on('leave')
+# def on_leave(data):
+#     username = data['username']
+#     room = data['room']
+#     leave_room(room)
+#     send(username + ' has left the room.', to=room)
 
 
 
