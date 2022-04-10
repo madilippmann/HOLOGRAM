2212d43d5e6189ef33c50d3232ea93bc902d9947
Madi Lippmann <madi.lippmann@gmail.com>
2022-04-10 11:16:40 -0700

    Styled message content bubbles

--- react-app/src/components/MessagePage/MessageContainer.js
+++ react-app/src/components/MessagePage/MessageContainer.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { getTimeElapsed } from '../../utils';
 import './MessageContainer.css'
-
+import defaultProfileImage from '../../static/default-profile-image.png'
 const MessageContainer = ({ thread, messages, onSubmit, message, setMessage, disabled }) => {
     const sessionUser = useSelector(state => state.session.user);
 
@@ -10,23 +10,37 @@ const MessageContainer = ({ thread, messages, onSubmit, message, setMessage, dis
     useEffect(() => {
         const messagesMap = document.getElementById("messages-map");
         messagesMap.scrollTop = messagesMap.scrollHeight - messagesMap.clientHeight;
-        console.log(messagesMap);
+        // console.log(messagesMap);
     }, [])
+
+    const threadUserNames = () => {
+        let users = Object.values(thread.users)
+        console.log('USERS: ', users)
+        const names = users.map(user => user.firstName)
+        return names.join(', ')
+    }
 
 
     return (
         <div className='messages-inner-container'>
             <div className='messages-header'>
-                <h2>Messages Container</h2>
-                <h3>{thread.name}</h3>
+                <h2 className='thread-name'>{threadUserNames()}</h2>
             </div>
 
             <div id='messages-map'>
                 {messages.map((message) => {
                     return (
                         <div key={message.id} className={message.userId === sessionUser.id ? 'me' : 'friend'}>
-                            <h4>{message.user.handle}</h4>
-                            <p>{message.content}</p>
+                            <div className='message__name-and-date'>
+                                <h4>{message.user.firstName}</h4>
+                                <h6>{getTimeElapsed(message.createdAt)}</h6>
+                            </div>
+                            <div className='message-and-avatar'>
+                                <div className='message__avatar'>
+                                    <img src={message.user.profileImageUrl !== 'default-profile-image.png' ? message.user.profileImageUrl : defaultProfileImage} alt='user-avatar' />
+                                </div>
+                                <p className='message__content'>{message.content}</p>
+                            </div>
                         </div>
                     );
                 })}
