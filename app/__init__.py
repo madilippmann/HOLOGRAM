33fb34ae91e1f1672cef539812fe06166ee454a6
Madi Lippmann <madi.lippmann@gmail.com>
2022-04-10 23:22:43 -0700

    Save before moving this over to a new branch

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager, current_user
-from flask_socketio import SocketIO
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
@@ -16,7 +15,7 @@ from .api.s3_routes import s3_routes
 from .api.threads_routes import threads_routes
 from .api.search_routes import search_routes
 
-from app.socketio import socketio
+from .socketio import socketio
 
 from .seeds import seed_commands
 
--- app/socketio.py
+++ app/socketio.py
@@ -4,12 +4,13 @@ from .models import User
 import os
 
 if os.environ.get('FLASK_ENV') == 'production':
-    origin = ['https://hologram--app.herokuapp.com', 'http://hologram--app.herokuapp.com']
+    origins = ['https://hologram--app.herokuapp.com', 'http://hologram--app.herokuapp.com']
 else:
-    origin = "*"
+    origins = "*"
 
 
-socketio = SocketIO(cors_allowed_origins="*", logger=True, engineio_logger=True, async_mode='eventlet')
+
+socketio = SocketIO(cors_allowed_origins=origins, async_mode='eventlet')
 
 
 # CONNECTION STUFF
