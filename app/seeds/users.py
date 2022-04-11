c84c0928ff1dd97915cbf7cc1bbbd8aebea00642
Kevin Pravia Mayorga <keserrano00@gmail.com>
2022-04-10 19:23:12 -0600

    add seeder data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -20,6 +20,58 @@ def seed_users():
 
     kayleigh = User(firstName="Kayleigh", lastName="Rupert", handle="baby_columbus", email="Brendan.Kub@hotmail.com", password='password', bio="Deleniti illo voluptas quia commodi. Sapiente in cum. Iure consequatur magnam non minus. Praesentium omnis est nostrum quae suscipit recusandae. Est et tempore praesentium rerum exercitationem.", profileImageUrl="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/111.jpg", privateStatus=False)
 
+    Antone = User(firstName = "Antonettem", lastName = "Block", handle = "heavy-waterfall-46", email = "after@gmail.com", password = "password", bio = "Yes, as every one knows, meditation and water are wedded for ever.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/ahmad-ossayli-GJ_Ecmrj0-E-unsplash+Large.jpeg")
+    Keyon = User(firstName = "Keyon", lastName = "Feest", handle = 'little-morning', email = "score_mariah@outlook.com", password = "password", bio = "Tell me, does the magnetic virtue of the needles of the compasses of all those ships attract them thither?", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/aldi-sigun-xyVIi4GN5Os-unsplash+Large.jpeg")
+    Tierra = User(firstName = "Tierra", lastName = "Schiller", handle = "slimy.moon.43", email = "men24@yahoo.com", password = "password", bio = "There now is your insular city of the Manhattoes, belted round by wharves as Indian isles by coral reefs—commerce surrounds it with her surf.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/alex-avalos-vExD0Y0Aei0-unsplash+Large.jpeg")
+    Merritt = User(firstName = "Merritt", lastName = "Greenholt", handle = "crazy.frost.99", email = "lennie@outlook.com", password = "password", bio = "Nice", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/ali-morshedlou-WMD64tMfc4k-unsplash+Large.jpeg")
+    Keshawn = User(firstName = "Keshawn", lastName = "Heathcote", handle = "miss-hill", email = "alias@hotmail.com", password = "password", bio = "Do you think the archangelGabriel thinks anything the less of me, because I promptly andrespectfully obey that old hunks in that particular instance?", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/austin-distel-7uoMmzPd2JA-unsplash+Large.jpeg")
+    Jayne = User(firstName = "Jayne", lastName = "Hahn", handle = "bold.tree.93", email = "short43@yahoo.com", password = "password", bio = "And,doubtless, my going on this whaling voyage, formed part of the grandprogramme of Providence that was drawn up a long time ago.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/ben-parker-OhKElOkQ3RE-unsplash+Large.jpeg")
+    Lucas = User(firstName = "Lucas", lastName = "Kuhic", handle = "nervous.voice.28", email = "trouble@gmail.com", password = "password", bio = "Are the green fields gone?", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/brooke-cagle-kvKSL7B6eTo-unsplash+Large.jpeg")
+    Camryn = User(firstName = "Camryn", lastName = "Schumm", handle = "hearty.haze.85", email = "top@gmail.com", password = "password", bio = "How then is this?", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/bruce-mars-8YG31Xn4dSw-unsplash+Large.jpeg")
+    Judd = User(firstName = "Judd", lastName = "Quitzon", handle = "moving-surf-61", email = "dangelo@yahoo.com", password = "password", bio = "The act of paying is perhaps the most uncomfortableinfliction that the two orchard thieves entailed upon us.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/cesar-rincon-XHVpWcr5grQ-unsplash+Large.jpeg")
+    Evalyn = User(firstName = "Evalyn", lastName = "Hintz", handle = "stormy.rain.26", email = "horse77@hotmail.com", password = "password", bio = "Go visit the Prairies in June,when for scores on scores of miles you wade knee-deep amongTiger-lilies—what is the one charm wanting?", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/charlesdeluvio-K4mSJ7kc0As-unsplash+Large.jpeg")
+
+    Kaitlin = User(firstName = "Kaitlin", lastName = "Jones", handle = "brave.dust", email = "brother_hassan@outlook.com", password = "password", bio = "And,doubtless, my going on this whaling voyage, formed part of the grandprogramme of Providence that was drawn up a long time ago.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/charlesdeluvio-kVg2DQTAK7c-unsplash+Large.jpeg")
+    Myra = User(firstName = "Myra", lastName = "Haag", handle = "eternal.wood", email = "rope34@gmail.com", password = "password", bio = "Finally, I always go to sea as a sailor, because of the wholesomeexercise and pure air of the fore-castle deck.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash+Large.jpeg")
+    Eldridge = User(firstName = "Eldridge", lastName = "Abbott", handle = "viscountess.meadow.22", email = "market45@yahoo.com", password = "password", bio = "It is the imageof the ungraspable phantom of life; and this is the key to it all.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/christopher-campbell-rDEOVtE7vOs-unsplash+Large.jpeg")
+    Emilie = User(firstName = "Emilie", lastName = "Kirlin", handle = "icy.smoke", email = "ivy@hotmail.com", password = "password", bio = "True, they rather order me about some, and make me jump from spar tospar, like a grasshopper in a May meadow.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/daria-pimkina-Dj5HnHMtkH0-unsplash+Large.jpeg")
+    Madaline = User(firstName = "Madaline", lastName = "Schoen", handle = "screaming-snow", email = "eos51@gmail.com", password = "password", bio = "The urbane activity with which a manreceives money is really marvellous, considering that we so earnestly believe money to be the root of all earthly ills, and that on noaccount can a monied man enter heaven.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/donn-gabriel-baleva-U-Z4P2H3KFE-unsplash+Large.jpeg")
+    Asa = User(firstName = "Asa", lastName = "Crona", handle = "worthy.bird", email = "olaf@outlook.com", password = "password", bio = "There now is your insular city of the Manhattoes, belted round by wharves as Indian isles by coral reefs—commerce surrounds it with her surf.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/dragos-gontariu-GH-mSApoKO0-unsplash+Large.jpeg")
+    August = User(firstName = "August", lastName = "Bogan", handle = "doc-morning-29", email = "speak_mackenzie@yahoo.com", password = "password", bio = "Again, I always go to sea as a sailor, because they make a point ofpaying me for my trouble, whereas they never pay passengers a singlepenny that I ever heard of.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/freestocks-9UVmlIb0wJU-unsplash+Large.jpeg")
+    Camren = User(firstName = "Camren", lastName = "Vandervort", handle = "pet-mountain", email = "size45@hotmail.com", password = "password", bio = "Besides, passengersget sea-sick—grow quarrelsome—don’t sleep of nights—do not enjoythemselves much, as a general thing", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/gabriel-salas-YnENabLdEKY-unsplash+Large.jpeg")
+    Hiram = User(firstName = "Hiram", lastName = "Sauer", handle = "captain.morning", email = "rosalia@yahoo.com", password = "password", bio = "Call me Ishmael.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/george-bohunicky-qJKT2rMU0VU-unsplash+Large.jpeg")
+    Hugh = User(firstName = "Hugh", lastName = "Mills", handle = "sunny-butterfly", email = "roberta@hotmail.com", password = "password", bio = "how cheerfully we consignourselves to perdition!", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/gian-cescon-00ByEXKcSkA-unsplash+Large.jpeg")
+
+    Reece = User(firstName = "Reece", lastName = "Gibson", handle = "tidy-dust", email = "dollar@outlook.com", password = "password", bio = "Chief among these motives was the overwhelming idea of the great whalehimself.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/gift-habeshaw-w1XuHvzcWoI-unsplash+Large.jpeg")
+    Dina = User(firstName = "Dina", lastName = "Gleason", handle = "boiling-meadow", email = "stevie@gmail.com", password = "password", bio = "Who ain’t happy?", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/haitham-PY2lJbqsJzU-unsplash+Large.jpeg")
+    Aylin = User(firstName = "Aylin", lastName = "Barton", handle = "forgotten.sound", email = "tobin@hotmail.com", password = "password", bio = "There now is your insular city of the Manhattoes, belted round by wharves as Indian isles by coral reefs—commerce surrounds it with her surf.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/haydn-golden-wcvuv00UDd4-unsplash+Large.jpeg")
+    Bert = User(firstName = "Bert", lastName = "Leffler", handle = "mellow-resonance-47", email = "tempore_audra@gmail.com", password = "password", bio = "Whenever I find myself growing grim about the mouth", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/hesam-jr-Pbci0Vb3CTk-unsplash+Large.jpeg")
+    Susie = User(firstName = "Susie", lastName = "Wehner", handle = "stoned.surf.84", email = "temporibus@outlook.com", password = "password", bio = "In much the same way do the commonalty lead their leaders in manyother things, at the same time that the leaders little suspect it.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/irina-SNUvxm6vM2I-unsplash+Large.jpeg")
+    Berta = User(firstName = "Berta", lastName = "Johns", handle = "eager-shadow-3", email = "raise75@yahoo.com", password = "password", bio = "It is out of the idolatrous dotings of the oldEgyptians upon broiled ibis and roasted river horse, that you see themummies of those creatures in their huge bake-houses the pyramids.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/joanna-nix-walkup-p5hKTIIJUjg-unsplash+Large.jpeg")
+    Cleveland = User(firstName = "Cleveland", lastName = "Wunsch", handle = "small.frost", email = "libby@gmail.com", password = "password", bio = "Ah!", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/kamran-ch-BgTc5D1HoCc-unsplash+Large.jpeg")
+    Ceasar = User(firstName = "Ceasar", lastName = "McCullough", handle = "hearty.wildflower.89", email = "of@hotmail.com", password = "password", bio = "The act of paying is perhaps the most uncomfortableinfliction that the two orchard thieves entailed upon us.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/kimson-doan-HD8KlyWRYYM-unsplash+Large.jpeg")
+    Donavon = User(firstName = "Donavon", lastName = "McGlynn", handle = "lively-wildflower-54", email = "ronny@hotmail.com", password = "password", bio = "Take almost any path you please, and ten to one it carries you down in a dale, and leaves you there by a pool in the stream.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/linkedin-sales-solutions-pAtA8xe_iVM-unsplash+Large.jpeg")
+    Lindsay = User(firstName = "Lindsay", lastName = "Leannon", handle = "color.wave", email = "estevan@gmail.com", password = "password", bio = "I should now take it into my head to go on a whaling voyage.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/linkedin-sales-solutions-pAtA8xe_iVM-unsplash+Large.jpeg")
+
+    Jalen = User(firstName = "Jalen", lastName = "Shields", handle = "white.grass", email = "element@hotmail.com", password = "password", bio = "Look at the crowds of water-gazers there.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/lucas-hoang-mwfBszKf5Xw-unsplash+Large.jpeg")
+    Mert = User(firstName = "Mert", lastName = "Kuphal", handle = "maxi-dew-91", email = "home40@yahoo.com", password = "password", bio = "Yet here they all unite.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/manh-nghiem-gKyweodgqXc-unsplash+Large.jpeg")
+    Amy = User(firstName = "Amy", lastName = "Bednar", handle = "spacy.glade.75", email = "lura@outlook.com", password = "password", bio = "True, they rather order me about some, and make me jump from spar tospar, like a grasshopper in a May meadow.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/matheus-ferrero-W7b3eDUb_2I-unsplash+Large.jpeg")
+    Clifford = User(firstName = "Clifford", lastName = "Glover", handle = "small-flower-62", email = "method91@hotmail.com", password = "password", bio = "Yes, as every one knows, meditation and water are wedded for ever.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/michael-dam-mEZ3PoFGs_k-unsplash+Large.jpeg")
+    Bernita = User(firstName = "Bernita", lastName = "Watsica", handle = "homeless.rain.81", email = "allow27@gmail.com", password = "password", bio = "And at first, this sort ofthing is unpleasant enough.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/nezt-xs-jfONSAqUTBM-unsplash+Large.jpeg")
+    Stewart = User(firstName = "Stewart", lastName = "Reynolds", handle = "knight.brook", email = "voice@outlook.com", password = "password", bio = "Do you think the archangelGabriel thinks anything the less of me, because I promptly andrespectfully obey that old hunks in that particular instance?", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/rafay-ansari-B8S0UAvuuuA-unsplash+Large.jpeg")
+    Laura = User(firstName = "Laura", lastName = "Hills", handle = "kiddo.glitter", email = "quia69@yahoo.com", password = "password", bio = "Call me Ishmael.", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/rafay-ansari-QUfY0hpULc0-unsplash+Large.jpeg")
+    Jerel = User(firstName = "Jerel", lastName = "Emard", handle = "solid-cherry-13", email = "compare@yahoo.com", password = "password", bio = "How then is this?", privateStatus=False, profileImageUrl = "https://hologram--app.s3.amazonaws.com/profile-image-seed/rick-gebhardt-_c7hWYN28m8-unsplash+Large.jpeg")
+    Jaydon = User(firstName = "Jaydon", lastName =  "Steuber", handle =  "rusty.snow.43", email =  "earth@gmail.com", password = "password", bio =  "Are the green fields gone?", privateStatus=False, profileImageUrl="https://hologram--app.s3.amazonaws.com/profile-image-seed/rick-gebhardt-3faX71BaNe0-unsplash+Large.jpeg")
+    Leo = User(firstName = "Leo", lastName =  "Pouros", handle =  "beau.hill", email =  "matter@hotmail.com", password = "password", bio =  "Yet here they all unite.", privateStatus=False, profileImageUrl="https://hologram--app.s3.amazonaws.com/profile-image-seed/rick-gebhardt-EOVaZYrquds-unsplash+Large.jpeg")
+
+    Clint = User(firstName = "Clint", lastName =  "Orn", handle =  "cool.river.39", email =  "et_meta@outlook.com", password = "password", bio =  "On the contrary, passengers themselves mustpay.", privateStatus=False, profileImageUrl="https://hologram--app.s3.amazonaws.com/profile-image-seed/simon-lee-kpvzAuCiS9U-unsplash+Large.jpeg")
+    Jason = User(firstName = "Jason", lastName =  "Stamm", handle =  "brutal.feather.13", email =  "no_russ@yahoo.com", password = "password", bio =  "I abandon the glory and distinctionof such offices to those who like them.", privateStatus=False, profileImageUrl="https://hologram--app.s3.amazonaws.com/profile-image-seed/sobhan-joodi-JCC88-aHEgk-unsplash+Large.jpeg")
+    Linda = User(firstName = "Linda", lastName =  "Howell", handle =  "throbbing.field", email =  "emmy@gmail.com", password = "password", bio =  "Why did the poor poet of Tennessee, uponsuddenly receiving two handfuls of silver, deliberate whether to buyhim a coat, which he sadly needed, or invest his money in a pedestriantrip to Rockaway Beach?", privateStatus=False, profileImageUrl="https://hologram--app.s3.amazonaws.com/profile-image-seed/sobhan-joodi-Nxck8QcAj84-unsplash+Large.jpeg")
+    Lenore = User(firstName = "Lenore", lastName =  "Jakubowski", handle =  "late.night", email =  "voluptate_jesus@yahoo.com", password = "password", bio =  "What do you see?", privateStatus=False, profileImageUrl="https://hologram--app.s3.amazonaws.com/profile-image-seed/tamara-bellis-A3Gd2b-98_g-unsplash+Large.jpeg")
+    Murphy = User(firstName = "Murphy", lastName =  "Cummings", handle =  "vital-brook-29", email =  "cool89@hotmail.com", password = "password", bio =  "Again, I always go to sea as a sailor, because they make a point ofpaying me for my trouble, whereas they never pay passengers a singlepenny that I ever heard of.", privateStatus=False, profileImageUrl="https://hologram--app.s3.amazonaws.com/profile-image-seed/taylor-wright-g0pb9aXpbgQ-unsplash+Large.jpeg")
+
+
+
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
@@ -28,7 +80,51 @@ def seed_users():
     db.session.add(theresia)
     db.session.add(clement)
     db.session.add(kayleigh)
-
+    db.session.add(Antone)
+    db.session.add(Keyon)
+    db.session.add(Tierra)
+    db.session.add(Merritt)
+    db.session.add(Keshawn)
+    db.session.add(Jayne)
+    db.session.add(Lucas)
+    db.session.add(Camryn)
+    db.session.add(Judd)
+    db.session.add(Evalyn)
+    db.session.add(Kaitlin)
+    db.session.add(Myra)
+    db.session.add(Eldridge)
+    db.session.add(Emilie)
+    db.session.add(Madaline)
+    db.session.add(Asa)
+    db.session.add(August)
+    db.session.add(Camren)
+    db.session.add(Hiram)
+    db.session.add(Hugh)
+    db.session.add(Reece)
+    db.session.add(Dina)
+    db.session.add(Aylin)
+    db.session.add(Bert)
+    db.session.add(Susie)
+    db.session.add(Berta)
+    db.session.add(Cleveland)
+    db.session.add(Ceasar)
+    db.session.add(Donavon)
+    db.session.add(Lindsay)
+    db.session.add(Jalen)
+    db.session.add(Mert)
+    db.session.add(Amy)
+    db.session.add(Clifford)
+    db.session.add(Bernita)
+    db.session.add(Stewart)
+    db.session.add(Laura)
+    db.session.add(Jerel)
+    db.session.add(Jaydon)
+    db.session.add(Leo)
+    db.session.add(Clint)
+    db.session.add(Jason)
+    db.session.add(Linda)
+    db.session.add(Lenore)
+    db.session.add(Murphy)
 
     db.session.commit()
 
@@ -41,3 +137,13 @@ def seed_users():
 def undo_users():
     db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
     db.session.commit()
+
+
+
+
+
+
+# https://hologram--app.s3.amazonaws.com/profile-image-seed/timothy-dykes-yd4ubMUNTG0-unsplash+Large.jpeg
+# https://hologram--app.s3.amazonaws.com/profile-image-seed/toa-heftiba-O3ymvT7Wf9U-unsplash+Large.jpeg
+# https://hologram--app.s3.amazonaws.com/profile-image-seed/vadim-bogulov-krgb_3HIkME-unsplash+Large.jpeg
+# https://hologram--app.s3.amazonaws.com/profile-image-seed/vika-strawberrika-9EWOktTMKFM-unsplash+Large.jpeg
