b14a8f035b35297c0156324c844c74a8deb96a3d
nick-esqueda <nick_esqueda@outlook.com>
2022-04-11 05:05:13 -0500

    added more padding to icons in footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -13,69 +13,69 @@ export default function Footer() {
 			<div id='footer-background' className='hidden'>
 			</div>
 			<footer id='footer'>
- 				<div id='information-button'>
- 					<button id='change' type='button' onClick={() => setShowInfo(!showInfo)}>i</button>
- 				</div>
- 				<div id='languages'>
-				 <ul>
- 						<li>
+				<div id='information-button'>
+					<button id='change' type='button' onClick={() => setShowInfo(!showInfo)}>i</button>
+				</div>
+				<div id='languages'>
+					<ul>
+						<li>
 							Madi Lippmann
 							<a href='https://github.com/madilippmann' target="_blank" rel="noreferrer">
-								<FontAwesomeIcon icon={faGithub} className='link-logo'/>
+								<FontAwesomeIcon icon={faGithub} className='link-logo' />
 							</a>
 							<a href='https://www.linkedin.com/in/madilippmann/' target="_blank" rel="noreferrer">
-								<FontAwesomeIcon icon={faLinkedin} className='link-logo'/>
+								<FontAwesomeIcon icon={faLinkedin} className='link-logo' />
 							</a>
 						</li>
- 						<li>
+						<li>
 							Nick Esqueda
 							<a href='https://github.com/nick-esqueda' target="_blank" rel="noreferrer">
-								<FontAwesomeIcon icon={faGithub} className='link-logo'/>
+								<FontAwesomeIcon icon={faGithub} className='link-logo' />
 							</a>
 							<a href='https://www.linkedin.com/in/nick-esqueda/' target="_blank" rel="noreferrer">
-								<FontAwesomeIcon icon={faLinkedin} className='link-logo'/>
+								<FontAwesomeIcon icon={faLinkedin} className='link-logo' />
 							</a>
 						</li>
- 						<li>
+						<li>
 							Kevin Pravia Mayorga
 							<a href='https://github.com/reversalbino' target="_blank" rel="noreferrer">
-								<FontAwesomeIcon icon={faGithub} className='link-logo'/>
+								<FontAwesomeIcon icon={faGithub} className='link-logo' />
 							</a>
 							<a href='https://www.linkedin.com/in/kevin-pravia-mayorga-51380817a' target="_blank" rel="noreferrer">
-								<FontAwesomeIcon icon={faLinkedin} className='link-logo'/>
+								<FontAwesomeIcon icon={faLinkedin} className='link-logo' />
 							</a>
 						</li>
- 					</ul>
- 				</div>
- 			</footer>
+					</ul>
+				</div>
+			</footer>
 		</>
 	)
-	:
-	(
-		<>
- 			<div id='footer-background' className='hidden'>
- 			</div>
- 			<footer id='footer'>
- 				<div id='information-button'>
- 					<button id='change' type='button' onClick={() => setShowInfo(!showInfo)}>i</button>
- 				</div>
- 				<div id='languages'>
- 					<ul>
- 						<li>React</li>
- 						<li>Redux</li>
- 						<li>Python</li>
- 						<li>Flask</li>
- 						<li>PostgreSQL</li>
- 						<li>SQLAlchemy</li>
- 						<li>Heroku</li>
- 						<li>AWS</li>
- 						<li>HTML</li>
- 						<li>CSS</li>
- 					</ul>
- 				</div>
- 			</footer>
- 		</>
-	)
+		:
+		(
+			<>
+				<div id='footer-background' className='hidden'>
+				</div>
+				<footer id='footer'>
+					<div id='information-button'>
+						<button id='change' type='button' onClick={() => setShowInfo(!showInfo)}>i</button>
+					</div>
+					<div id='languages'>
+						<ul>
+							<li>React</li>
+							<li>Redux</li>
+							<li>Python</li>
+							<li>Flask</li>
+							<li>PostgreSQL</li>
+							<li>SQLAlchemy</li>
+							<li>Heroku</li>
+							<li>AWS</li>
+							<li>HTML</li>
+							<li>CSS</li>
+						</ul>
+					</div>
+				</footer>
+			</>
+		)
 }
 
 // export default function Footer() {
